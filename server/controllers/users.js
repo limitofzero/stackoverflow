ed4264e190fe0eb6c0cded82e0a835990c439ea4
limitofzero <limitofzero@gmail.com>
2020-07-26 21:54:02 +0300

    refactor(server): remove nested promises in login method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -33,34 +33,27 @@ module.exports = {
     const result = {};
     let status = 200;
 
-    mongoose.connect(connUri, { useNewUrlParser: true }).then(() => {
-      User.findOne({ email }).then((user) => {
-        bcrypt.compare(password, user.password).then(match => {
-          if (match) {
-            const payload = { email: user.email, name: user.name };
-            const options = { expiresIn: '2d' };
-            const secret = process.env.JWT_SECRET;
-            const token = jwt.sign(payload, secret, options);
+    mongoose.connect(connUri, { useNewUrlParser: true })
+      .then(() => User.findOne({ email }))
+      .then(user => {
+        const match = bcrypt.compare(password, user.password);
+        if (match) {
+          const payload = { email: user.email, name: user.name };
+          const options = { expiresIn: '2d' };
+          const secret = process.env.JWT_SECRET;
+          const token = jwt.sign(payload, secret, options);
 
-            result.token = token;
-            result.status = status;
-          } else {
-            status = 401;
-            result.result = status;
-            result.error = 'Auth error';
-          }
+          result.token = token;
+          result.status = status;
+        } else {
+          status = 401;
+          result.result = status;
+          result.error = 'Auth error';
+        }
 
-          res.status(status)
-            .send(result);
-        }).catch(err => send500Status(res, err));
-
-      }).catch(err => {
-        status = 404;
-        result.status = status;
-        result.error = err;
         res.status(status)
           .send(result);
       })
-    }).catch(err => send500Status(res, err));
+      .catch(err => send500Status(res, err));
   }
 };
