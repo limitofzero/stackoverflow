1f68c4acd7c108646955819384345d1f09398dd3
limitofzero <limitofzero@gmail.com>
2020-09-19 21:11:13 +0300

    feat() add microservice

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { AppModule } from "./app/app.module";
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  app.listen(() => console.log('Microservice is listening'));
 }
-
 bootstrap();
