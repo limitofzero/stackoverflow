67de32536699eedf7ddf5d6af9c89305de8c53a8
limitofzero <limitofzero@gmail.com>
2020-10-04 21:17:31 +0300

    feat(): add global prefix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 
